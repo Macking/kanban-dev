865c38f681ef354a5524d05d8e45ac1f95ef237c
Macking Liu <macking@autodesk.com>
2015-08-26 13:41:41 +0800

    Chapter 8

--- app/main.jsx
+++ app/main.jsx
@@ -1,22 +1,19 @@
-require('./main.css');
-//var component = require('./component');
-//var app = document.createElement('div');
-//document.body.appendChild(app);
-//app.appendChild(component());
-
 import 'array.prototype.findindex';
+import './main.css';
+
+import React from 'react';
+import App from './components/App.jsx';
 import alt from './libs/alt';
 import storage from './libs/storage';
 import persist from './libs/persist';
-import React from 'react';
-import App from './components/App.jsx';
 
 main();
 
 function main() {
-	persist(alt, storage, 'app');
+  persist(alt, storage, 'app');
+  const app = document.createElement('div');
 
-	const app = document.createElement('div');
-	document.body.appendChild(app);
-	React.render(<App />, app);
+  document.body.appendChild(app);
+
+  React.render(<App />, app);
 }
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -3,59 +3,68 @@ import alt from '../libs/alt';
 import NoteActions from '../actions/NoteActions';
 
 class NoteStore {
-    constructor() {
-        this.bindActions(NoteActions);
+  constructor() {
+    this.bindActions(NoteActions);
 
-        this.notes = this.notes || [];
+    this.notes = this.notes || [];
+
+    this.exportPublicMethods({
+      get: this.get.bind(this)
+    });
+  }
+  create(note) {
+    const notes = this.notes;
+
+    note.id = uuid.v4();
+
+    this.setState({
+      notes: notes.concat(note)
+    });
+  }
+  update({id, task}) {
+    let notes = this.notes;
+    const noteIndex = this.findNote(id);
+
+    if(noteIndex < 0) {
+      return;
     }
 
-    create(note) {
-        const notes = this.notes;
+    notes[noteIndex].task = task;
 
-        note.id = uuid.v4();
+    this.setState({notes});
+  }
+  delete(id) {
+    const notes = this.notes;
+    const noteIndex = this.findNote(id);
 
-        this.setState({
-            notes: notes.concat(note)
-        });
+    if(noteIndex < 0) {
+      return;
     }
 
-    update({id, task}) {
-        let notes = this.notes;
-        const noteIndex = this.findNote(id);
+    this.setState({
+      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
+    });
+  }
+  findNote(id) {
+    const notes = this.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
 
-        if (noteIndex < 0) {
-            return;
-        }
-
-        notes[noteIndex].task = task;
-
-        this.setState({notes});
-
+    if(noteIndex < 0) {
+      console.warn('Failed to find note', notes, id);
     }
 
-    delete(id) {
-        const notes = this.notes;
-        const noteIndex = this.findNote(id);
+    return noteIndex;
+  }
+  get(ids) {
+    const notes = this.notes || [];
+    const notesIds = notes.map((note) => note.id);
 
-        if (noteIndex < 0) {
-            return;
-        }
-
-        this.setState({
-            notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
-        });
+    if(ids) {
+      return ids.map((id) => notes[notesIds.indexOf(id)]);
     }
 
-    findNote(id) {
-        const notes = this.notes;
-        const noteIndex = notes.findIndex((note) => note.id === id);
-
-        if (noteIndex < 0) {
-            console.warn('Fail to find note', notes, id);
-        }
-
-        return noteIndex;
-    }
+    return [];
+  }
 }
 
 export default alt.createStore(NoteStore, 'NoteStore');
